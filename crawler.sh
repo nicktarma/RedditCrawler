a32c5cd2416b25488da424dad50a2f26aafa1624
Gopal <gopalknookula@gmail.com>
2023-05-05 18:17:37 -0700

    Changes to the bash script

--- crawler.sh
+++ crawler.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#install necessary libraries
+# install necessary libraries
 pip3 install praw urllib3 beautifulsoup4
-#python -m pip install urllib3
-#running the script
-#nohup runs it in the background
+
+# running the script
+# nohup runs it in the background
 #> /dev/null ignores output
-nohup python3 praw-test.py > /dev/null 2>&1 &
+
+nohup python3 praw-test.py $1 $2 > /dev/null 2>&1 &
